7800101560a2816942061ffb8f4ab62e6ac89d42
Tony Collins <123tonycollins@gmail.com>
2024-10-06 13:23:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Blank_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
